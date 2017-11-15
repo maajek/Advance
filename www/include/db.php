190aeea8832e5c9b799a2478c970f90728838950
maajek <majekodunmi>
2017-11-15 14:25:20 -0800

    setting up development environment

---
+++ www/include/db.php
@@ -0,0 +1,14 @@
+<?php
+
+   define('DBNAME','store');
+   define('DBUSER','root');
+   define('DBPASS','majek');
+  
+   try{
+	   
+	   $conn = new PDO('mysql:host=localhost;dbname'.DBNAME,DBUSER,DBPASS);
+	   $conn ->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_SILENT);
+   }catch(PDOException $err){
+	   echo $err->getMessage();
+	 }
+ ?>
