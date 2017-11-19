f6cdb59ae99c1812349a4236ce5c05ad1748296c
maajek <majekodunmi>
2017-11-19 14:14:19 -0800

    login page work

--- www/login.php
+++ www/login.php
@@ -2,30 +2,81 @@
 
 	 $page_title = "Login";
   include 'include/header.php';
+  include 'include/db.php';
 
+  include 'include/functions.php';
+      $error = [];
+      
+      if(array_key_exists('register', $_POST)) {
+        
+      
+      if(empty($_POST['email'])){
+         $error['email'] = "Please enter you email" ;
+      }
+
+      if(empty($_POST['password'])) {
+      	 $error['password']= "Please enter your password";
+      }
+
+      if(empty($error)) {
+        //do data base stuff
+
+       /* $stmt = $conn->prepare("SELECT FROM admin(email,hash)VALUES(:e,:h)");
+         
+        $data =[ 
+            ":e"=> $email,
+            ":h"=> $hash
+        ];
+           
+        $stmt -> execute($data);*/
+       $stmt = $conn->prepare("SELECT email FROM admin WHERE :e=email");
+       $stmt -> bindParam(":e" , $email);
+       $stmt->execute();
+      
+       $stmt = $conn->prepare("SELECT password FROM admin WHERE :h=hash");
+       $stmt -> bindParam(":h" , $hash);
+       $stmt->execute();
+
+      header ("location:testing.php");
+
+
+       }
+
+     }
 
 	?>
 
 
 
 	<div class="wrapper">
-		<h1 id="register-label">Register</h1>
+		<h1 id="register-label">Admin Login</h1>
 		<hr>
-		<form id="register"  action ="register.php" method ="POST">
+		<form id="register"  action ="login.php" method ="POST">
 			<div>
-				<label>first name:</label>
-				<input type="text" name="fname" placeholder="first name">
-			</div>
-			<div>
-				<label>last name:</label>	
-				<input type="text" name="lname" placeholder="last name">
-			</div>
+				 <?php 
+                 $data = displayErrors($error,'email');
+                 echo $data;
 
-			<div>
+			    ?>
 				<label>email:</label>
 				<input type="text" name="email" placeholder="email">
 			</div>
 			<div>
+				 <?php 
+                 $data = displayErrors($error,'password');
+                 echo $data;
+
+			     ?>
+				<label>password:</label>
+				<input type="password" name="password" placeholder="password">
+			</div>
+
+			<input type="submit" name="register" value="login">
+		</form>
+
+		<h4 class="jumpto">Don't have an account? <a href="register.php">register</a></h4>
+	</div>
+
 	<?php
   include 'include/footer.php';
 
